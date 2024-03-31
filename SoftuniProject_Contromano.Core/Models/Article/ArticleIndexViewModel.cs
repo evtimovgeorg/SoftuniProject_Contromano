429190c6455956e667421fbf625ba600178699d1
Georg Evtimov <evtimovgeorg@hotmail.com>
2024-03-31 21:23:07 +0200

    Create Article FormModel

---
+++ SoftuniProject_Contromano.Core/Models/Article/ArticleIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftuniProject_Contromano.Core.Models.Article
+{
+    public class ArticleIndexViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = String.Empty;
+        public string Content { get; set; } = String.Empty;
+        public string ImageUrl { get; set; } = String.Empty;
+        public string Author { get; set; } = String.Empty;
+        public DateTime CreatedOn { get; set; }
+
+
+    }
+}
