fdb7350a66061e25f8c2fcc141c3cb333f5cc033
Georg Evtimov <evtimovgeorg@hotmail.com>
2024-04-02 23:58:07 +0200

    Implement Interface

--- SoftuniProject_Contromano.Core/Contracts/IArticleService.cs
+++ SoftuniProject_Contromano.Core/Contracts/IArticleService.cs
@@ -1,12 +1,16 @@
-﻿using System;
+﻿using SoftuniProject_Contromano.Core.Models.Article;
+using SoftuniProject_Contromano.Infrastucture.Data.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SoftuniProject_Contromano.Core.Contracts
 {
     public interface IArticleService
     {
+        Task<ArticleDetailsServiceModel> GetArticleById(int id);
+        IEnumerable<Article> GetAllArticles();
+        Task SaveArticle(Article article);
+        Task EditArticle(Article article);
+        Task DeleteArticle(int id);
     }
 }
